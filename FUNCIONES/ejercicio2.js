1cc43d0615c1cc3af02739c64c8e2c44a0977627
ALP300 <aitorpalomino1@gmail.com>
2024-11-24 20:07:27 -0500

    Realizar ejercicios de funciones

---
+++ FUNCIONES/ejercicio2.js
@@ -0,0 +1,7 @@
+/* 
+Ejercicios básicos con funciones
+
+a) Función para calcular el cuadrado de un número
+b) Función para verificar si un número es par
+c) Función que reciba un array y devuelva su longitud
+*/
